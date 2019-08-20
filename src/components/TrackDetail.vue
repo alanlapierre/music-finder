7d7b8b25710c59f8c4af01fd6a7edbf27ec3a4c5
aelapierre <lapierre82@gmail.com>
2019-08-20 19:16:42 -0300

    Se agregan páginas de Detalle de Canción y Acerca de Nosotros.

---
+++ src/components/TrackDetail.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .container
+    .columns
+      .column.is-5.is-offset-4
+        app-track(:track="track")
+</template>
+
+<script>
+import trackService from '@/services/track'
+import AppTrack from '@/components/Track.vue'
+
+export default {
+  components: { AppTrack },
+
+  data () {
+    return {
+      track: {}
+    }
+  },
+
+  created () {
+    const id = this.$route.params.id
+
+    trackService.getById(id)
+      .then(res => {
+        this.track = res
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .column {
+    margin: 20px;
+  }
+</style>
