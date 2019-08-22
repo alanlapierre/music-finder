a188b4695c474051309510321b74f66a5ae1bec1
alanlapierre <lapierre82@gmail.com>
2019-08-22 16:49:24 -0300

    Se agrega custom directive para blurear resultados que no tengan preview de audio y no puedan reproducirse.

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -18,9 +18,9 @@
         small {{ track.duration_ms | ms-to-mm }}
         nav.level
           .level-left
-            a.level-item
+            button.button.is-primary
               span.icon.is-small(@click="selectTrack") ▶️
-            a.level-item
+            button.button.is-warning
               span.icon.is-small(@click="goToTrack(track.id)") 🌎  
 </template>
 
@@ -31,10 +31,12 @@ export default {
   },
   methods: {
     selectTrack () {
+      if (!this.track.preview_url) { return }
       this.$emit('select', this.track.id)
       this.$bus.$emit('set-track', this.track)
     },
     goToTrack (id) {
+      if (!this.track.preview_url) { return }
       this.$router.push({ name: 'track', params: { id } })
     }
   }
