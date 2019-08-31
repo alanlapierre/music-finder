ed91408fc11e3499835697aae1010e249dafb1b8
aelapierre <lapierre82@gmail.com>
2019-08-31 10:03:35 -0300

    Mejoras en el estilo del header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,30 @@
 <template lang="pug">
   section.hero.is-success
     .hero-head
-      header.nav
+      header.navbar
         .container
-          .nav-left
-            .nav-item
-              strong Music Finder
-            .nav-right.nav-menu
-              a.nav-item(@click="selectLang('en')") En
-              a.nav-item(@click="selectLang('es')") Es
-              router-link.nav-item(:to="{ name: 'search' }") {{ $t('search') }}
-              router-link.nav-item(to="about") {{ $t('about') }}
+          .navbar-brand
+            a.navbar-burger.burger(aria-label="menu", aria-expanded="false")
+              span(aria-hidden="true")
+              span(aria-hidden="true")  
+              span(aria-hidden="true")
+          .navbar-menu
+            .navbar-start
+              router-link.navbar-item(:to="{ name: 'search' }") {{ $t('search') }}
+              router-link.navbar-item(to="about") {{ $t('about') }}
+            .navbar-end
+              .field.navbar-item
+                .control.has-icons-left
+                  .select.is-rounded.is-small
+                    select(@change="selectLang()", v-model="lang")
+                      option(value='en' selected) En
+                      option(value='es') Es
+                  .icon.is-small.is-left
+                    i.fas.fa-flag
     .hero-body
       .container.has-text-centered
-        h1.title Platzi Music
-        h2.subtitle Canciones que estan Vue-nisimas!
+        h1.title.is-bold MusicFinder
+        h2.subtitle Buscador de Canciones Avanzado
         app-player
 </template>
 
@@ -23,9 +33,14 @@ import AppPlayer from '@/components/Player.vue'
 
 export default {
   components: { AppPlayer },
+  data () {
+    return {
+      lang: 'en'
+    }
+  },
   methods: {
-    selectLang (lang) {
-      this.$i18n.locale = lang
+    selectLang () {
+      this.$i18n.locale = this.lang
     }
   }
 }
